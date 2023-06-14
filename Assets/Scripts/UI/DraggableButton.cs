ebef5d67c007b85f290bbf7925487b2db7d12897
hyungjin <skrt1533@gmail.com>
2023-06-14 20:39:32 +0900

    making changes on DraggableButton.cs

--- Assets/Scripts/UI/DraggableButton.cs
+++ Assets/Scripts/UI/DraggableButton.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DraggableButton : EventTrigger
+public class DraggableButton : MonoBehaviour
 {
     private RectTransform _rectTransform;
    
@@ -15,13 +16,14 @@ public class DraggableButton : EventTrigger
     [SerializeField]
     private float maxDist = 500f;
 
-    private bool canDrag = true;
+    private bool canDrag = false;
 
     //드래그하고 있는 거리
     private float dragDist = 0f;
     private Vector2 mouseDragStartPos = Vector2.zero;
     private Vector2 mouseDragEndPos = Vector2.zero;
-    
+    private bool _buttonPressed = false;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -32,10 +34,42 @@ public class DraggableButton : EventTrigger
     }
 
     // Update is called once per frame
-    void Update()
-    {}
+    private void FixedUpdate() {
+        
+        //모바일에서는 터치패드 방식으로 여러 터치 입력을 받아 처리합니다.
+        
+        HandleTouchInput();
+        //모바일이 아닌PC나 유니티 에디터상에서 작동할 때는 터치 입력이 아닌 마우스로 입력받는다.
+        
+        #if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_WEBPLAYER
 
-    public override void OnBeginDrag(PointerEventData eventData)
+            HandleInput(Input.mousePosition);
+
+        #endif
+    }
+
+    private void HandleTouchInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleInput(Vector3 mousePosition)
+    {
+        mouseDragStartPos = mousePosition;
+    }
+
+    public void ButtonDown() {
+        _buttonPressed = true;
+    }
+
+    public void ButtonUp() {
+        _buttonPressed = false;
+
+        //버튼이 떼어졌을 때 터치패드와 촤표를 원래 지점으로 복귀시킵니다.
+        HandleInput(_startPos);
+    }
+
+/*     public override void OnBeginDrag(PointerEventData eventData)
     {
         base.OnBeginDrag(eventData);
         mouseDragStartPos = Input.mousePosition;
@@ -45,9 +79,7 @@ public class DraggableButton : EventTrigger
     {
         base.OnDrag(eventData);
         if(canDrag) {
-            dragDist = Input.mousePosition.y - mouseDragStartPos.y;
-            // if(dragDist >= maxDist) dragDist = maxDist;
-            // if(dragDist <= 0) dragDist = 0;
+            dragDist = (Input.mousePosition.y - mouseDragStartPos.y) * 3f;
 
             _rectTransform.localPosition = new Vector3(_startPos.x, 
                                                     _startPos.y + Mathf.Clamp(dragDist, 0, maxDist), 
@@ -61,6 +93,6 @@ public class DraggableButton : EventTrigger
         base.OnEndDrag(eventData);
         _rectTransform.localPosition = _startPos;
         mouseDragEndPos = Input.mousePosition;
-    }
+    } */
 
 }
